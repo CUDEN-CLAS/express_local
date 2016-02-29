25acbe6735cb9695932847a9dbfb93747ce8f16f
James W Fuller <James.W.Fuller@Colorado.EDU>
2016-02-29 14:29:35 -0700

    Switch to single bundle.

--- ansible/roles/behat/scripts/behat_params.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-BEHAT_PARAMS='{"extensions":{"Drupal\\DrupalExtension":{"drupal":{"drupal_root":"/data/web/express/testing"},"drush":{"root":"/data/web/express/testing"}},"Behat\\MinkExtension":{"base_url":"http://express.local/testing"}}}'
-export BEHAT_PARAMS
