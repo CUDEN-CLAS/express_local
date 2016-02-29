a201a587a57fc0d9ce708e599f3b67cb528cd957
James W Fuller <James.W.Fuller@Colorado.EDU>
2016-02-29 13:11:41 -0700

    Tests run locally.

--- ansible/roles/behat/scripts/behat_params.sh
+++ ansible/roles/behat/scripts/behat_params.sh
@@ -1,6 +1,3 @@
 #! /bin/sh
-BEHAT_PARAMS='{"extensions":{"Drupal\\DrupalExtension":{"drupal":{"drupal_root":"/data/web/express/BEHAT_TESTING_PATH"},"drush":{"root":"/data/web/express/BEHAT_TESTING_PATH"}},"Behat\\MinkExtension":{"base_url":"http://express.local/BEHAT_TESTING_PATH"}}}'
-BEHAT_PARAMS=`echo $BEHAT_PARAMS | sed -e s#BEHAT_TESTING_PATH#$BEHAT_TESTING_PATH#`
-BEHAT_PARAMS=`echo $BEHAT_PARAMS | sed -e s#BEHAT_TESTING_PATH#$BEHAT_TESTING_PATH#`
-BEHAT_PARAMS=`echo $BEHAT_PARAMS | sed -e s#BEHAT_TESTING_PATH#$BEHAT_TESTING_PATH#`
+BEHAT_PARAMS='{"extensions":{"Drupal\\DrupalExtension":{"drupal":{"drupal_root":"/data/web/express/testing"},"drush":{"root":"/data/web/express/testing"}},"Behat\\MinkExtension":{"base_url":"http://express.local/testing"}}}'
 export BEHAT_PARAMS
